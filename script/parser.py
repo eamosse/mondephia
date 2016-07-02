338c69bb707b282d07c99a1a7e8f5448b39ab0e6
Ghislain <atemezin@eurecom.fr>
2016-07-02 18:47:31 +0200

    added first version of onto

---
+++ script/parser.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul  2 16:42:39 2016
+
+@author: aedouard
+"""
+
+import pandas
+import json
+import vincent
+ 
+dates_ITAvWAL = []
+# f is the file pointer to the JSON data set
+f = open('/Users/aedouard/Dropbox/_dev/HackaTAL2016/Tweets/Matchs/train_euro2016/Groupe_A/fr/France_Albanie_2016-06-15_21h_fr.json')
+i = 0 
+for line in f:
+    tweet = json.loads(line)
+    #print(tweet)
+    if 'created_at' in tweet:
+        dates_ITAvWAL.append(tweet['created_at'])
+    
+# a list of "1" to count the hashtags
+ones = [1]*len(dates_ITAvWAL)
+# the index of the series
+idx = pandas.DatetimeIndex(dates_ITAvWAL)
+# the actual series (at series of 1s for the moment)
+ITAvWAL = pandas.Series(ones, index=idx)
+# Resampling / bucketing
+per_minute = ITAvWAL.resample('1T').sum()#.fillna(0)  
+
+time_chart = vincent.Line(per_minute)
+time_chart.axis_titles(x='Time', y='Freq')
+time_chart.to_json('time_chart.json')     
+    
+"""       
+    # let's focus on hashtags only at the moment
+    terms_hash = [term for term in preprocess(tweet['text']) if term.startswith('#')]
+    # track when the hashtag is mentioned
+    if '#itavwal' in terms_hash:
+        dates_ITAvWAL.append(tweet['created_at'])
+ 
+# a list of "1" to count the hashtags
+ones = [1]*len(dates_ITAvWAL)
+# the index of the series
+idx = pandas.DatetimeIndex(dates_ITAvWAL)
+# the actual series (at series of 1s for the moment)
+ITAvWAL = pandas.Series(ones, index=idx)
+ 
+# Resampling / bucketing
+per_minute = ITAvWAL.resample('1Min', how='sum').fillna(0)
+"""

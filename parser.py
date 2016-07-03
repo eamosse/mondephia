78f721505e62eb794e1d725eb241ca29ca019735
Amosse Edouard <eamosse@gmail.com>
2016-07-03 11:47:54 +0200

    Retrouver les pics

--- parser.py
+++ parser.py
@@ -12,15 +12,17 @@ import utils
 dates_ITAvWAL = []
 from dateutil.parser import parse
 from datetime import datetime
+import spike_finder 
+file= '/Users/aedouard/Dropbox/_dev/HackaTAL2016/Tweets/Matchs/train_euro2016/Groupe_F/fr/Portugal_Autriche_2016-06-18_21h_fr.json'
 # f is the file pointer to the JSON data set
-f = open('/Users/aedouard/Dropbox/_dev/HackaTAL2016/Tweets/Matchs/train_euro2016/Groupe_E/fr/Belgique_Italie_2016-06-13_21h_fr.json')
+f = open(file)
 i = 0 
 for line in f:
     tweet = json.loads(line)
     #print(tweet)
-    if 'created_at' in tweet:
+    if 'created_at' in tweet and 'retweeted_status' not in tweet:
         dates_ITAvWAL.append(tweet['created_at'])
-    
+f.close()   
 # a list of "1" to count the hashtags
 ones = [1]*len(dates_ITAvWAL)
 # the index of the series
@@ -28,12 +30,13 @@ idx = pandas.DatetimeIndex(dates_ITAvWAL)
 # the actual series (at series of 1s for the moment)
 ITAvWAL = pandas.Series(ones, index=idx)
 # Resampling / bucketing
-per_minute = ITAvWAL.resample('30S').sum().fillna(0)  
+per_minute = ITAvWAL.resample('1Min').sum().fillna(0)  
 
 time_chart = vincent.Line(per_minute)
 time_chart.axis_titles(x='Time', y='Freq')
 time_chart.to_json('time_chart.json')    
 utils.process('time_chart.json')
+data = spike_finder.findTweets(file)
 
         
     
